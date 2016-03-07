f8141bc0a12964d9d9607ab6c2e591ff6cfd3cc9
Anusha Balakrishnan <anusha28@rye01.stanford.edu>
2016-03-06 23:44:34 -0800

    add args for dataset size and num frames, randomly sample clips

--- src/modeling/average_frame.py
+++ src/modeling/average_frame.py
@@ -12,6 +12,8 @@ def main(args):
     dataset_json = args.json
     tuning_layers = args.tuning_layers
     vgg_path = args.vgg
+    max_items = args.max_items
+    max_frames = args.max_frames
     print "Loading VGGNet model"
 
     model = AverageFrameModel(vgg_path,
@@ -19,7 +21,7 @@ def main(args):
                               tuning_layers=tuning_layers)
 
     start = datetime.datetime.now()
-    data = read_dataset(dataset_json, sample_probability=0.5, max_items=50, max_frames=20, mean_value=model.mean_bgr)
+    data = read_dataset(dataset_json, sample_probability=0.5, max_items=max_items, max_frames=max_frames, mean_value=model.mean_bgr)
     print data["train_X"].shape
     end = datetime.datetime.now()
     print "Read data in %d seconds" % (end-start).seconds
@@ -28,7 +30,8 @@ def main(args):
                                 data["train_X"], data["train_y"],
                                 val_X=data["val_X"], val_y=data["val_y"],
                                 num_epochs=5,
-                                batch_size=5)
+                                batch_size=2,
+                                output_lr=1e-6)
     solver.train()
 
 if __name__ == "__main__":
@@ -38,6 +41,8 @@ if __name__ == "__main__":
                              ' corresponding labels for each video. See sample_dataset.json for an example.')
     parser.add_argument('--vgg', type=str, default=DEFAULT_MODEL_PATH, help='Path to weights for pretrained VGG16 model (in .pkl format)')
     parser.add_argument('--tune', type=str, action='append', dest='tuning_layers', help='Name of layer(s) to tune weights for. This argument must be provided one for each layer separately. For example, python average_frame.py --tune fc7 --tune fc8 will tune the parameters for fc7 and fc8.')
+    parser.add_argument('--max_items', type=int, default=10, help='Max items to load from the dataset')
+    parser.add_argument('--max_frames', type=int, default=20, help='Max frames to load per clip')
     clargs = parser.parse_args()
 
     main(clargs)
